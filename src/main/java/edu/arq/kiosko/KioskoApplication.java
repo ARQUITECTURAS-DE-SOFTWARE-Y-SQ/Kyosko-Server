fec43e5032fe6045bc99fa1baf623418efb5ca85
jhon joya <jjoyaquintero@gmail.com>
2025-08-24 17:20:37 -0500

    se sube proyecto base recuperado de initializer

---
+++ src/main/java/edu/arq/kiosko/KioskoApplication.java
@@ -0,0 +1,13 @@
+package edu.arq.kiosko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskoApplication.class, args);
+	}
+
+}
